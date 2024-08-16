f957513abc0514caebb8e428bdbf6b4dc8911574
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2024-08-16 19:31:54 -0300

    add prepare script prisma generate

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,34 +2,35 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { prisma } from "./prisma";
- 
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   pages: {
-    signIn: '/login',
-    signOut: '/login',
-    verifyRequest: '/login',
-    error: '/login',
+    signIn: "/login",
+    signOut: "/login",
+    verifyRequest: "/login",
+    error: "/login",
   },
   providers: [
     Credentials({
-        authorize: async (credentials: {email: string, password: string}) => {
-          const user = await prisma.user.findUnique({
-            where: { 
-                email: credentials.email,
-                password: credentials.password
-            },
-          });
+      authorize: async (credentials: { email: string; password: string }) => {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+            password: credentials.password,
+          },
+        });
 
-          if(user != null) {
-            return user
-          }
+        if (user != null) {
+          return user;
+        }
 
-          return null;
-        },
-      }),
+        return null;
+      },
+    }),
   ],
   session: {
-    strategy: 'jwt'
+    strategy: "jwt",
   },
-})
+  secret: process.env.AUTH_SECRET,
+});
