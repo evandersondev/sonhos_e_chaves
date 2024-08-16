6220bbdd09bd312992c842ae93ba8afeaea224dd
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2024-08-16 19:54:58 -0300

    add suspense component in index page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,19 +1,22 @@
-import { AboutUs } from '@/components/about-us'
-import { Footer } from '@/components/footer'
-import { Hero } from '@/components/hero'
-import { Immobile } from '@/components/immobile'
-import { Separator } from '@/components/ui/separator'
+import { AboutUs } from "@/components/about-us";
+import { Footer } from "@/components/footer";
+import { Hero } from "@/components/hero";
+import { Immobile } from "@/components/immobile";
+import { Separator } from "@/components/ui/separator";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
-    <div className="bg-white">
-      <Hero />
-      <Separator orientation="horizontal" className="h-[1px] bg-zinc-100" />
-      <Immobile />
-      <Separator orientation="horizontal" className="h-[1px] bg-zinc-100" />
-      <AboutUs />
-      <Separator orientation="horizontal" className="h-[1px] bg-zinc-100" />
-      <Footer />
-    </div>
-  )
+    <Suspense fallback={<div />}>
+      <div className="bg-white">
+        <Hero />
+        <Separator orientation="horizontal" className="h-[1px] bg-zinc-100" />
+        <Immobile />
+        <Separator orientation="horizontal" className="h-[1px] bg-zinc-100" />
+        <AboutUs />
+        <Separator orientation="horizontal" className="h-[1px] bg-zinc-100" />
+        <Footer />
+      </div>
+    </Suspense>
+  );
 }
