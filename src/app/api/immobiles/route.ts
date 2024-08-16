314329758952ab540ee558f512b39354b4073bf2
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2024-08-16 19:45:17 -0300

    update immobiles api route again

--- src/app/api/immobiles/route.ts
+++ src/app/api/immobiles/route.ts
@@ -2,44 +2,41 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const q = searchParams.get("");
+  const rooms = searchParams.get("");
+  const bathrooms = searchParams.get("");
+  const carage = searchParams.get("");
+  const type = searchParams.get("");
+  const page = Number(searchParams.get("")) || 1;
+  const limit = 8;
+  const skip = (Number(page) - 1) * limit;
+  const filters = {};
+
+  if (q) {
+    filters["OR"] = [
+      { address: { contains: q, mode: "insensitive" } },
+      { name: { contains: q, mode: "insensitive" } },
+    ];
+  }
+
+  if (rooms) {
+    filters["rooms"] = parseInt(rooms);
+  }
+
+  if (bathrooms) {
+    filters["bathrooms"] = parseInt(bathrooms);
+  }
+
+  if (carage) {
+    filters["carage"] = parseInt(carage);
+  }
+
+  if (type) {
+    filters["type"] = type;
+  }
+
   try {
-    const searchParams = request.nextUrl.searchParams;
-
-    const q = searchParams.get("");
-    const rooms = searchParams.get("");
-    const bathrooms = searchParams.get("");
-    const carage = searchParams.get("");
-    const type = searchParams.get("");
-    const page = Number(searchParams.get("")) || 1;
-
-    const limit = 8;
-    const skip = (Number(page) - 1) * limit;
-
-    const filters = {};
-
-    if (q) {
-      filters["OR"] = [
-        { address: { contains: q, mode: "insensitive" } },
-        { name: { contains: q, mode: "insensitive" } },
-      ];
-    }
-
-    if (rooms) {
-      filters["rooms"] = parseInt(rooms);
-    }
-
-    if (bathrooms) {
-      filters["bathrooms"] = parseInt(bathrooms);
-    }
-
-    if (carage) {
-      filters["carage"] = parseInt(carage);
-    }
-
-    if (type) {
-      filters["type"] = type;
-    }
-
     const immobiles = await prisma.immobile.findMany({
       where: filters,
       take: limit,
